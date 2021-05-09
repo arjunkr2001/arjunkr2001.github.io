f843630c347967430c92d41d8d985fceb7a9c0e6
arjunkr2001 <68550118+arjunkr2001@users.noreply.github.com>
2021-05-09 15:04:56 +0530

    Update searchJS.js

--- web/search/searchJS.js
+++ web/search/searchJS.js
@@ -1,6 +1,6 @@
 function google(){
 	var term = document.getElementById("trm").value;
-	if(term!=''){
+	if(term!='' || document.querySelector('.box').style.width!=40){
 		window.location="https://duckduckgo.com/?q="+escape(term);
 	}
 }
